ec02d6cc6ffdb0a3ff55e27fa63fb6a0ca8346aa
2Dvrss33 <mgdss950@gmail.com>
2024-11-19 11:57:40 -0300

    n

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Charada',
+    ' O que é, o que é? Tem pescoço e não tem cabeça, tem braços e não tem mãos, tem corpo e não tem pernas',
+
+    'A camisa.'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? Cai de pé e corre deitado',
+    'A chuva'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? O nadador faz para bater o recorde',
+    'Nada'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? Quanto maior menos se vê.?',
+    'A escuridão.'
+)
